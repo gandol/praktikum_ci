e60a4d94a98b66854969f940362ae3e6ce56451e
gandol <suro.gento4@gmail.com>
2019-10-21 14:05:54 +0700

    commit pertama

---
+++ application/views/nama_saya.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html>
+	<title>Nama Author</title>
+	<body>
+		<h3>Ahmad Gika Darmawan</h3>
+	</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Selamat datang di CodeIgniter</title>
+</head>
+<body>
+	<h1>Praktikum Codeigniter</h1>
+</body>
+</html>
